98683703c4a11b8452c9ed28a6936c7c7fa3489a
Mic Hussey <mic@confluent.io>
2018-10-09 21:43:15 +0200

    Generated by Spring Initializr

---
+++ src/main/java/io/confluent/examples/scs_demo/ScsDemoApplication.java
@@ -0,0 +1,12 @@
+package io.confluent.examples.scs_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScsDemoApplication.class, args);
+	}
+}
